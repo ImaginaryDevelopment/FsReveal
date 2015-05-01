81c3ce29e8bd899eb124c79a7b97c915eb29e374
bdimpe@gmail <bdimpe@gmail>
2015-05-01 10:33:05 -0400

    added more robust error handling to build release.cmd

---
+++ build release.cmd
@@ -0,0 +1,20 @@
+@echo off
+cls
+
+.paket\paket.bootstrapper.exe
+if %ERRORLEVEL% NEQ 0 (
+  pause
+  exit /b %errorlevel%
+)
+
+.paket\paket.exe restore -v
+if %ERRORLEVEL% NEQ 0 (
+  pause
+  exit /b %errorlevel%
+)
+
+packages\FAKE\tools\FAKE.exe build.fsx RELEASE %*
+if %ERRORLEVEL% NEQ 0 (
+  pause
+  exit /b ERRORLEVEL%
+)
