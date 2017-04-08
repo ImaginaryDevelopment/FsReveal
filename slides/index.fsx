da84fc1ec3973d87316a533d000e1c3fa7c276af
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2017-04-08 11:01:30 -0400

    some improvements and more C# comparisons based on newer versions of C#

--- slides/index.fsx
+++ slides/index.fsx
@@ -7,6 +7,16 @@
 
 ***
 
+### FsReveal notes #
+
+- [http://fsprojects.github.io/FsReveal/getting-started.html](http://fsprojects.github.io/FsReveal/getting-started.html)
+- `Esc` to see overview
+- `f` to view in fullscreen
+- `s` to see speaker notes
+
+***
+***
+
 ### INTRO TO F#
 
 #### (For C#ers)
@@ -23,15 +33,14 @@
 
 *)
 open System
-let a = 5 // var a = 5;
-let c = 1 + a // var c = 1 + a;
-// public int Factorial(int x) => Enumerable.Range(1, x)
-//  .Aggregate((x1,x2) => x1 * x2);
-let factorial x = [1..x] |> List.reduce (*)
-// var d = Factorial(a);
-let d = factorial a
+module Syntax = 
+  let a = 5 // var a = 5;
+  let c = 1 + a // var c = 1 + a;
+  // public int Twice(int x) => x * 2;
+  let twice x = 2 * x
+  let d = twice a
 (**
-`c` and `d` are evaluated for you
+fsreveal magic `c` and `d` are evaluated for you
 *)
 (*** include-value: c ***)
 (*** include-value: d ***)
@@ -47,7 +56,8 @@ module Assignment =
   // = is a binding or comparison not an Assignment
   let x = 5 // not mutable
   let mutable y = 5
-  let z = x = 6
+  // parens are just to help visualize it for new learners
+  let z = (x = 6)
   y <- y + 1
 
 (**
@@ -62,7 +72,8 @@ module Assignment =
 *)
 // string F1(int x) => x < 5 ? "less than" : "not less than";
 let f1 x = if x < 5 then "less than" else "not less than"
-// string F2(int lower, int x, int upper) => x < lower ? "less than" : x > upper ? "greater than" : "between";
+// string F2(int lower, int x, int upper) => 
+//    x < lower ? "less than" : x > upper ? "greater than" : "between";
 let f2 lower x upper =
   if x < lower then
     "less than"
@@ -81,14 +92,16 @@ module Casting =
   // var y = (obj) x;
   // upcast (always succeeds if it compiles)
   let y:obj = x :> obj
-  // var z = (int)y;
   // downcast when the type isn't inferrable (? mark notes the possibility of failure)
+  // var z = (int)y;
   let z:int = y :?> int
   // var a = (obj)x;
   let a:obj = upcast x // when the type is inferrable
   // var b = (int)y;
   // when the type is inferrable (still could fail)
   let b:int = downcast y
+  // var x2 = (object)2;
+  let x2:obj = upcast 2
 (**
 ---
 
@@ -96,16 +109,22 @@ module Casting =
 *)
 
 module Casting2 =
-  // var x = (object)2;
-  let x:obj = upcast 2
-  // can't conditionally define a variable nor have it be the exact type
+  // box is a shorthand for upcast to object
+  let x:obj = box 2
+  // in C#5(maybe even 6) you can't conditionally define a variable
   // var a2 = x as int?;
   // if(a2 != null) Console.WriteLine("int value:" + a2);
   // else Console.WriteLine(x.ToString());
   match x with
   | :? int as a -> printfn "int value: %i" a
-  | foo when isNull foo -> printfn "non-int null value is %A" foo // calls ToString if it is not null, a tuple, record or union
+  // calls ToString if it is not null, a tuple, record or union
+  | null -> printfn "non-int null value is %A" x // would print <null> for null values
+  // conditional matches
+  | :? string as str when str.Length > 0 -> printfn "string value: %s" str
+  // also prints <null> for null values
   | x -> printfn "nonNull value is %O" x // calls toString if it is not null
+  // advanced/arguably misusable power: 
+  // in F# we don't need to use a separate variable name for the casted variable
 
 (**
 ***
@@ -113,30 +132,38 @@ module Casting2 =
 #### fields vs methods
 *)
 module FExamples =
-  let x = 1; // static field
+  // readonly static int x = 1;
+  let x = 1 // static field
   // int Y() => 1;
   let y () = 1 // method
   // void Z() {}
   let z () = ()
   // void F(int x) {}
-  let f x = (); // method
+  let f (x:int) = () // method
   // void F2(int x) => f(x);
-  let f2 = f // method
+  let f2 x = f x // method
 
 (**
 
 ---
 #### Objects
+    [lang=cs]
+    public class Employee
+    {
+      int x="hello";
+      public int Foo() => x;
+      public int Z => x;
+      public static void Bar() => {};
+    }
+    public class Foo{}
 *)
-type Foo() = class end
-// public class HelloClass {
 type HelloClass() =
-  //int x = "hello";
   let x = "hello" // field
-  // public Foo() { return x;}
   member this.Foo() = x
   member this.Z = x
   static member Bar() = ()
+// empty class
+type Foo() = class end
 (**
 ---
 #### Interfaces
@@ -145,28 +172,53 @@ type IAmAnInterface =
   abstract member Bark : unit -> string
   abstract member Foo : string with get
   abstract member Bar : string with get,set
-
+(**
+---
+*)
 type ClassImplements () =
   let x = "hello"
   // you can use whatever name you like for `this`
   member __.Bark() = x
-  // is a property! not a field
+  // Foo is a property! not a field
   member __.Foo = x
   member __.Foo2 with get() = x
   member val Bar = x with get,set
 
+  // f# downside no implicit interface implementation
+  // interface members are always considered explicity implemented
+  // members only show up/compile if you cast the type to the interface first
   interface IAmAnInterface with
     member __.Foo = x
-    member x.Bark () = x.Bark()
-    member x.Bar with get() = x.Bar and set v = x.Bar <- v
+    member this.Bark () = this.Bark()
+    member this.Bar 
+      with get() = this.Bar 
+      and set v = this.Bar <- v
 
 (**
 ---
+#### C#
+    [lang=cs]
+    public class Employee
+    {
+      public string Name { get; } // C# 6 syntax
+      public Guid EmployeeId { get; }
+      public decimal Salary { get;}
+
+      public Employee(string name, Guid employeeId, decimal salary)
+        Name=name;
+        EmployeeId=employeeId;
+        Salary=salary;
+      }
+    }
+
+
 #### Records
 *)
 
 type Employee = {Name:string; EmployeeId:Guid; Salary:decimal}
 let e = {Name="Brandon D'Imperio"; EmployeeId=Guid.NewGuid(); Salary = 15.5m}
+// how much typing would you need to do in C# to do this?
+// create a new employee with the same salary ... and any other properties that we don't set get copied.
 let e2 = {e with Name="John Doe"; EmployeeId = Guid.NewGuid()}
 
 (**
@@ -177,11 +229,16 @@ let e2 = {e with Name="John Doe"; EmployeeId = Guid.NewGuid()}
 *)
 module SequenceExamples =
   // new is not required in F#
+  // you will may get a warning though if you don't use new on IDisposables
   let a = System.Collections.Generic.List<int>()
-  let literalArray = [| |]
+  // same as the line above (using build-in f# aliases)
+  let b = ResizeArray<int>()
+  let literalArray = [| |] // also Array.empty
+  // var arrayOf1To10 = Enumerable.Rangea(1, 10).ToArray();
   let arrayOf1To10 = [| 1..10 |]
+  // F# list type
   let literalList = [] // F# version is immutable
-  let listOf1To10 = [ 1..10]
+  let listOf1To10 = [ 1..10 ]
 
 (**
 ---
